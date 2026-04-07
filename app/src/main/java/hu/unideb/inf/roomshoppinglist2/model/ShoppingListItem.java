7bc2a30692142b6a017b78ad2012af6a805d5cad
Kocsis Gergely <kocsisger@gmail.com>
2026-04-07 10:48:37 +0200

    Entity

---
+++ app/src/main/java/hu/unideb/inf/roomshoppinglist2/model/ShoppingListItem.java
@@ -0,0 +1,34 @@
+package hu.unideb.inf.roomshoppinglist2.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "ShoppingList")
+public class ShoppingListItem {
+    @PrimaryKey
+    @NonNull
+    int id;
+    String name;
+
+    @Override
+    public String toString() {
+        return id + " - " + name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
