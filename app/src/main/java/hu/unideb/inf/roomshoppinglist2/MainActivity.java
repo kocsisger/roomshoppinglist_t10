3a6d0d82cbbcbcb8103a747a0dc7bd1ee5d5d393
Kocsis Gergely <kocsisger@gmail.com>
2026-04-07 11:15:36 +0200

    Test 1

--- app/src/main/java/hu/unideb/inf/roomshoppinglist2/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/roomshoppinglist2/MainActivity.java
@@ -1,14 +1,21 @@
 package hu.unideb.inf.roomshoppinglist2;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
+
+import hu.unideb.inf.roomshoppinglist2.model.ShoppingListDatabase;
+import hu.unideb.inf.roomshoppinglist2.model.ShoppingListItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShoppingListDatabase shoppingListDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        shoppingListDatabase = Room.databaseBuilder(this, ShoppingListDatabase.class, "shoppinglist_db")
+                .fallbackToDestructiveMigration(true)
+                .build();
+
+        ShoppingListItem sli = new ShoppingListItem();
+        sli.setId(1);
+        sli.setName("Apple");
+        shoppingListDatabase.shoppingListDAO().insertListItem(sli);
+
+        Log.d("ChckDB", shoppingListDatabase.shoppingListDAO().getAllItems().toString());
     }
 }
--- app/src/main/java/hu/unideb/inf/roomshoppinglist2/model/ShoppingListDatabase.java
+++ app/src/main/java/hu/unideb/inf/roomshoppinglist2/model/ShoppingListDatabase.java
@@ -1,8 +1,9 @@
 package hu.unideb.inf.roomshoppinglist2.model;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 @Database(entities = ShoppingListItem.class, version = 1, exportSchema = false)
-public abstract class ShoppingListDatabase {
+public abstract class ShoppingListDatabase extends RoomDatabase {
     public abstract ShoppingListDAO shoppingListDAO();
 }
